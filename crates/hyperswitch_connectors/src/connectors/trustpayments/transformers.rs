34355ff9200386be792a4cc6444001b4d742c707
aadityaguptaa <aditya.gupta.001@juspay.in>
2025-09-24 17:59:09 +0530

    fixes clippy_v2 error

--- crates/hyperswitch_connectors/src/connectors/trustpayments/transformers.rs
+++ crates/hyperswitch_connectors/src/connectors/trustpayments/transformers.rs
@@ -1015,7 +1015,7 @@ impl TrustpaymentsPaymentResponseData {
                             common_enums::AttemptStatus::Charged
                         }
                         None => common_enums::AttemptStatus::Authorized,
-                    };
+                    }
                 } else if self.requesttypedescription == "TRANSACTIONQUERY"
                     && self.authcode.is_none()
                     && self.records.is_some()
@@ -1045,14 +1045,14 @@ impl TrustpaymentsPaymentResponseData {
                             common_enums::AttemptStatus::Charged
                         }
                         None => common_enums::AttemptStatus::Authorized,
-                    };
+                    }
 
                 } else {
                     common_enums::AttemptStatus::Pending
                 }
             }
             _ => {
-                self.errorcode.get_attempt_status();
+                self.errorcode.get_attempt_status()
             }
         };
 
